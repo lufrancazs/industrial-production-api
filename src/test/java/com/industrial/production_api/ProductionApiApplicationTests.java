d8b609989f757e80957ed74f84c9cf090e0f304e
Lucas Martins <lucasfranca_zs@hotmail.com>
2026-03-08 22:59:19 -0300

    initial project setup with Spring Initializr

---
+++ src/test/java/com/industrial/production_api/ProductionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.industrial.production_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
